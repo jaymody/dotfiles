a7dc604c6b548f90b877c3762b0082cfc163bd56
jaymody <jaykmody@gmail.com>
2020-01-18 17:49:27 -0500

    removed z script in favour of z plugin in ohmyzsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,7 +1,7 @@
 # oh-my-zsh
-export ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh" 
+export ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
-plugins=(git virtualenv docker pyenv python screen gcloud zsh-autosuggestions)
+plugins=(z git virtualenv docker pyenv python screen gcloud zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
 source ~/.p10k.zsh
 
