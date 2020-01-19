a6ba05a991cdd414bb249a634f01b204a94e07f1
jaymody <jaykmody@gmail.com>
2020-01-18 21:37:39 -0500

    added better touch tool settings

---
+++ btt/install.sh
@@ -0,0 +1,1 @@
+# unfortunately, there is no way to import btt settings via command line, you'll have to import the preset manually
---
+++ btt/save.sh
@@ -0,0 +1,1 @@
+# unfortunately, there is no way to export btt settings via command line, you'll have to save the preset manually
