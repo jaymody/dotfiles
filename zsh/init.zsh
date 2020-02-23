4e447b68d3aba16417c8a5d5a132e79bc8d22191
jaymody <jaykmody@gmail.com>
2020-02-22 20:18:10 -0500

    don't load oh-my-zsh aliases

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,10 +1,18 @@
-# oh-my-zsh
+# oh-my-zsh settings
 export ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(z git virtualenv docker pyenv python screen gcloud zsh-autosuggestions)
+
+# load oh-my-zsh while ignoring aliases
+save_aliases=$(alias -L)
 source $ZSH/oh-my-zsh.sh
+unalias -m '*'
+eval $save_aliases
+unset save_aliases
+
+# load powerlevel10k config
 source ~/.p10k.zsh
 
-# title
+# terminal title
 export DISABLE_AUTO_TITLE="true"
 echo -e "\033];terminal\007"
