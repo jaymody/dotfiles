cb1c3b94c2bfe19590dfb39cb9d32d0b3baa74d3
jaymody <jaykmody@gmail.com>
2020-04-12 16:41:17 -0400

    fix zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,7 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSLo install-ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh" sh install-ohmyzsh.sh
+export ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh"
+sh install-ohmyzsh.sh
 rm install-ohmyzsh.sh
 
 # install plugins/themes
