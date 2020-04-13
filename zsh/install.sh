5814653029cd7dfa9df3e19ecd402cd7bf81e518
jaymody <jaykmody@gmail.com>
2020-04-12 20:07:46 -0400

    updated install script to keep .zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,8 @@
 # install oh-my-zsh
-sh -c "$(curl -fsSLo install-ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 export ZSH="$DOTFILES_ROOT/zsh/oh-my-zsh"
+export KEEP_ZSHRC=yes
+export RUNZSH=no
+curl -fsSLo install-ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install-ohmyzsh.sh
 rm install-ohmyzsh.sh
 
