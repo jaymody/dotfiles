f9cc1dabe652a1786ef4ace4339933eadd3a62d4
jaymody <jaykmody@gmail.com>
2020-06-10 16:27:34 -0400

    updated docker aliases

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,4 +1,4 @@
 alias dockerpsa="docker ps -a"
 alias dockerpse='docker ps --filter "status=exited"'
-alias dockerent="docker exec -it /bin/bash"
-alias dockerents="docker run -it --entrypoint /bin/bash"
+alias dockerent="docker exec -it "
+alias dockerents="docker run -it "
