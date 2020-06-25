907cc36625bad048898b8c97ce03df91b06f0195
jaymody <jaykmody@gmail.com>
2020-06-24 22:49:38 -0400

    updated remoteurl alias to only print url

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,2 +1,2 @@
 alias remote='open -n $WEBBROWSER --args --new-tab $(git config --get remote.origin.url)'
-alias remoteurl='git config --get remote.origin.url | pbcopy'
+alias remoteurl='git config --get remote.origin.url'
