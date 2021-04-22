7f7d5fdee0e0874328e15fded312a2224c67b535
jaymody <jaykmody@gmail.com>
2021-04-22 14:59:01 -0400

    don't sync alfred, we on powerpack now bois

--- macos/update.sh
+++
@@ -1,8 +0,0 @@
-# check os is darwin (macos)
-if test "$(uname)" != "Darwin"; then
-    exit
-fi
-
-# alfred
-rm -rf ${DOTFILES_ROOT}/macos/alfred
-cp -r "${HOME}/Library/Application Support/Alfred/Alfred.alfredpreferences/preferences" ${DOTFILES_ROOT}/macos/alfred
