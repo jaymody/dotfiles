527acffcb9df330ce45db3799756cd741c0ce26b
Jay Mody <jaykmody@gmail.com>
2025-04-28 01:56:51 -0400

    temp commit, neovim changes

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,3 +1,5 @@
+if vim.fn.has "nvim-0.11" == 0 then error "This config requires Neovim 0.11+" end
+
 ----------------------------------
 --- global constants/functions ---
 ----------------------------------
@@ -18,26 +20,18 @@ local function setup_options()
   vim.g.mapleader = " "
   vim.g.maplocalleader = " "
 
-  vim.opt.number = true             -- show line numbers
-
-  vim.opt.ignorecase = true         -- ignore case in search
-  vim.opt.smartcase = true          -- unless the search has a capital
-
-  vim.opt.inccommand = "split"      -- preview substitutions as you type
-
+  vim.opt.number = true
+  vim.opt.ignorecase = true
+  vim.opt.smartcase = true
+  vim.opt.inccommand = "split"
   vim.opt.clipboard = "unnamedplus" -- use system clipboard
-
-  vim.opt.wrap = false              -- don't wrap lines
-
+  vim.opt.wrap = false
   vim.opt.shortmess:append("I")     -- don't show neovim splash screen message
-
-  vim.opt.confirm = true            -- confirm rather than failing for :q on unsaved changes
-
+  vim.opt.confirm = true
   vim.opt.signcolumn = "yes"        -- always show sign column to avoid it popping in/out
-
-  vim.opt.cursorline = true         -- highlight the line the cursor is on
-
-  vim.opt.undofile = true           -- persist undos
+  vim.opt.cursorline = true
+  vim.opt.undofile = true
+  vim.opt.showmode = false
 end
 
 -------------
@@ -48,21 +42,17 @@ local function setup_keymaps()
   -- use <C-c> as <Esc>
   kset({ "i", "c", "n", "v" }, "<C-c>", "<Esc>", { remap = true })
 
-  -- scroll up/down
-  kset({ "n", "v" }, "<C-j>", "<C-e>")
-  kset({ "n", "v" }, "<C-k>", "<C-y>")
-
   -- clear highlighting when pressing <Esc> in normal mode
-  kset("n", "<Esc>", ":noh<CR><C-c>")
+  kset("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
   -- when pasting in visual mode, do not override the yank register
   kset("v", "p", "\"_dP", { noremap = true, silent = true })
 
   -- jump straight to init.lua
-  kset("n", "<leader>c", run("e ~/.config/nvim/init.lua"))
+  kset("n", "<leader>cc", run("e ~/.config/nvim/init.lua"))
 
   -- reload init.lua
-  kset("n", "<leader>r", run("luafile ~/.config/nvim/init.lua"))
+  kset("n", "<leader>cr", run("luafile ~/.config/nvim/init.lua"))
 end
 
 -------------
@@ -72,9 +62,8 @@ end
 local function setup_autocmd()
   -- highlight yanked contents
   vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
     callback = function()
-      vim.highlight.on_yank { higroup = "Visual", timeout = 60 }
+      vim.highlight.on_yank { timeout = 40 }
     end,
   })
 
@@ -84,6 +73,63 @@ local function setup_autocmd()
     callback = function()
       vim.cmd("wincmd L")
     end,
+  })
+end
+
+---------
+-- lsp --
+---------
+
+local function setup_lsp()
+  vim.g.have_nerd_font = true
+  vim.opt.winborder = 'rounded'
+
+  vim.lsp.config['luals'] = {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        }
+      }
+    }
+  }
+  vim.lsp.enable('luals')
+
+
+  vim.lsp.config('*', {
+    capabilities = {
+      textDocument = {
+        semanticTokens = {
+          multilineTokenSupport = true,
+        }
+      }
+    },
+    root_markers = { '.git' },
+  })
+
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+      -- Auto-format ("lint") on save.
+      -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+      if not client:supports_method('textDocument/willSaveWaitUntil')
+          and client:supports_method('textDocument/formatting') then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          end,
+        })
+      end
+    end,
+  })
+
+  vim.diagnostic.config({
+    virtual_lines = { current_line = true }
   })
 end
 
@@ -137,227 +183,9 @@ local function fzf_lua_plugin()
           kset("n", "<leader>fc", run("FzfLua registers ''"))
           kset("n", "<leader>fb", run("FzfLua buffers ''"))
           kset("n", "<leader>fd", run("FzfLua lsp_workspace_diagnostics ''"))
-          kset("n", "gr", run("FzfLua lsp_references ''"))
-          kset("n", "ga", run("FzfLua lsp_code_actions ''"))
+          -- kset("n", "gr", run("FzfLua lsp_references ''"))
+          -- kset("n", "ga", run("FzfLua lsp_code_actions ''"))
         end
-  }
-end
-
--------------------------------------------------------------------------------
-
-local function cmp_plugin()
-  return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline"
-    },
-
-    config =
-        function()
-          local cmp = require("cmp")
-
-          local function make_mapping()
-            IN_CMP = false
-            local next_item = function(fallback)
-              if cmp.visible() then
-                IN_CMP = true
-                cmp.select_next_item()
-              else
-                cmp.abort()
-                fallback()
-              end
-            end
-            local prev_item = function(fallback)
-              if cmp.visible() then
-                IN_CMP = true
-                cmp.select_prev_item()
-              else
-                cmp.abort()
-                fallback()
-              end
-            end
-            local select_item = function(fallback)
-              if IN_CMP then
-                IN_CMP = false
-                cmp.confirm()
-              else
-                cmp.abort()
-                fallback()
-              end
-            end
-            local cancel_cmp = function(fallback)
-              if cmp.visible() then
-                IN_CMP = false
-                cmp.abort()
-              else
-                fallback()
-              end
-            end
-
-            return next_item, prev_item, select_item, cancel_cmp
-          end
-
-          local mapping = (function()
-            local next_item, prev_item, select_item, cancel_cmp = make_mapping()
-            return {
-              ["<Tab>"] = { i = next_item },
-              ["<S-Tab>"] = { i = prev_item },
-              ["<CR>"] = { i = select_item },
-              ["<C-BS>"] = { i = cancel_cmp },
-            }
-          end)()
-
-          local mapping_cmdline = (function()
-            local next_item, prev_item, select_item, cancel_cmp = make_mapping()
-            return {
-              ["<Tab>"] = { c = next_item },
-              ["<S-Tab>"] = { c = prev_item },
-              ["<CR>"] = { c = select_item },
-              ["<C-BS>"] = { c = cancel_cmp },
-            }
-          end)()
-
-          cmp.setup({
-            snippet = {
-              expand = function(args)
-                -- NOTE: with nvim v0.10, we can get rid of luasnip and just use
-                -- vim.snippet.expand(args.body), but I'm using luasnip here since
-                -- I may need to use older versions of nvim
-                require("luasnip").lsp_expand(args.body)
-              end,
-            },
-            mapping = mapping,
-            sources = cmp.config.sources({
-              { name = "nvim_lsp" },
-              { name = "path" },
-            }),
-          })
-
-          cmp.setup.cmdline({ "/", "?" }, {
-            mapping = mapping_cmdline,
-            sources = { { name = "buffer" } },
-          })
-
-          cmp.setup.cmdline(":", {
-            mapping = mapping_cmdline,
-            sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-            matching = { disallow_symbol_nonprefix_matching = false },
-          })
-        end
-  }
-end
-
--------------------------------------------------------------------------------
-
-local function lspconfig_plugin()
-  local function setup_lspconfig()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(event)
-        local opts = { buffer = event.buf }
-
-        kset("n", "gh",
-          function()
-            vim.lsp.buf.hover()
-            vim.diagnostic.open_float(0, { scope = "cursor" })
-          end, opts)
-
-        kset("n", "]d",
-          function()
-            vim.diagnostic.goto_next({ wrap = true, float = true })
-          end, opts)
-
-        kset("n", "[d",
-          function()
-            vim.diagnostic.goto_prev({ wrap = true, float = true })
-          end, opts)
-
-        kset("n", "gd", run("lua vim.lsp.buf.definition()"), opts)
-        kset("n", "gD", run("lua vim.lsp.buf.declaration()"), opts)
-        kset("n", "gi", run("lua vim.lsp.buf.implementation()"), opts)
-        kset("n", "go", run("lua vim.lsp.buf.type_definition()"), opts)
-        kset("n", "gs", run("lua vim.lsp.buf.signature_help()"), opts)
-        kset("n", "gR", run("lua vim.lsp.buf.rename()"), opts)
-        kset("n", "<F3>", run("lua vim.lsp.buf.format({async = true})"), opts)
-      end
-    })
-
-    --------------------
-    -- format on save --
-    --------------------
-    -- https://lsp-zero.netlify.app/docs/getting-started
-    local buffer_autoformat = function(bufnr)
-      local group = "lsp_autoformat"
-      vim.api.nvim_create_augroup(group, { clear = false })
-      vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          -- note: do not enable async formatting
-          vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-        end,
-      })
-    end
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(event)
-        local id = vim.tbl_get(event, "data", "client_id")
-        local client = id and vim.lsp.get_client_by_id(id)
-        if client == nil then
-          return
-        end
-
-        -- make sure there is at least one client with formatting capabilities
-        if client.supports_method("textDocument/formatting") then
-          buffer_autoformat(event.buf)
-        end
-      end,
-    })
-
-    -- diagnostics
-    vim.diagnostic.config({
-      signs = false,
-      virtual_text = false,
-      float = true,
-      update_in_insert = false,
-      underline = true
-    })
-  end
-
-  local function setup_lsp_servers()
-    local lspconfig_defaults = require('lspconfig').util.default_config
-    lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-      'force',
-      lspconfig_defaults.capabilities,
-      require('cmp_nvim_lsp').default_capabilities()
-    )
-
-    local lsp = require("lspconfig")
-    lsp.ocamllsp.setup({})
-    lsp.ruff.setup({})
-    lsp.rust_analyzer.setup({})
-    lsp.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
-  end
-
-  local function setup_mason()
-    require("mason").setup()
-  end
-
-  return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "williamboman/mason.nvim"
-    },
-    config = function()
-      setup_lspconfig()
-      setup_lsp_servers()
-      setup_mason()
-    end
   }
 end
 
@@ -409,8 +237,8 @@ local function gitsigns_plugin()
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      kset("n", "[c", do_and_center('Gitsigns prev_hunk'))
-      kset("n", "]c", do_and_center('Gitsigns next_hunk'))
+      kset("n", "<M-[>", do_and_center('Gitsigns prev_hunk'))
+      kset("n", "<M-]>", do_and_center('Gitsigns next_hunk'))
       kset("n", "<leader>gi", run('Gitsigns preview_hunk_inline'))
       kset("n", "<leader>gp", run('Gitsigns preview_hunk'))
       kset("n", "<leader>gr", run('Gitsigns reset_hunk'))
@@ -450,6 +278,30 @@ local function nvim_tree_plugin()
   }
 end
 
+-------------------------------------------------------------------------------
+
+local function onedark_theme()
+  return {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require('onedark').setup { style = 'darker' }
+      require('onedark').load()
+    end
+  }
+end
+
+-------------------------------------------------------------------------------
+
+local function surround_plugin()
+  return {
+    "tpope/vim-surround",
+    config = function()
+      kset("v", "s", "<Plug>VSurround")
+    end
+  }
+end
+
 ------------
 --- main ---
 ------------
@@ -477,12 +329,11 @@ local function setup_plugins()
     spec = {
       treesitter_plugin(), -- syntax highlighting
       fzf_lua_plugin(),    -- search
-      cmp_plugin(),        -- completions popup
-      lspconfig_plugin(),  -- lsp and completions
       gitsigns_plugin(),   -- git gutter and hunk manipulation/navigation
       readline_plugin(),   -- emacs like bindings for basic text stuff
       nvim_tree_plugin(),  -- file explorer because :Ex is very feature poor
-      { "tpope/vim-surround" }
+      surround_plugin(),   -- easily surround things in brackets
+      onedark_theme(),     -- theme
     }
   })
 end
@@ -495,6 +346,7 @@ local function main()
   setup_keymaps()
   setup_autocmd()
   setup_plugins()
+  setup_lsp()
 end
 
 main()
